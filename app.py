804a9b0fcbaaf423b4c7b91afe710a20152fa375
София Кучерова <sonyakucherova@yandex.ru>
2025-09-24 16:50:45 +0700

    Картинка

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, url_for
 
 app = Flask(__name__)
 
@@ -40,5 +40,17 @@ def author():
           </body>
         </html>"""
 
+@app.route('/image')
+def image():
+    path=url_for("static", filename="дуб.jpg")
+    return'''
+<!doctype html>
+<html>
+    <body>
+        <h1>Дуб</h1>
+        <img src="'''+ path +'''">
+    </body>
+</html>
+'''
 if __name__ == "__main__":
     app.run(debug=True)
