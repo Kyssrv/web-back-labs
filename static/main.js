92f688cf38fc603a431030d3f8881468001e2348
София Кучерова <sonyakucherova@yandex.ru>
2025-12-22 21:46:49 +0700

    Проверка описания

--- static/main.js
+++ static/main.js
@@ -93,9 +93,18 @@ function fillFilmList(){
                     headers: {"Content-Type": "application/json"},
                     body: JSON.stringify(film)
                 })
-                .then(function(){
-                    fillFilmList();
-                    hideModal();
+                .then(function(resp){
+                    if(resp.ok){
+                        fillFilmList();
+                        hideModal();
+                        return();
+                    }
+                    return resp.json();
+                })
+                .then(function(console.errors{
+                    if(errors.description)
+                        document.getElementById('description-error').innerText=errors.description;
+
                 });
             }
 
